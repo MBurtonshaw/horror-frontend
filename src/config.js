926afe9da0115939caef9187389a2bd120beec6f
Matt <MBurtonshaw@gmail.com>
2025-08-15 16:43:44 -0400

    url mismatch

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     //apiBaseUrl: 'http://localhost:8080'
-    apiBaseUrl: 'https://horror-catalogue-9e055c6b0745.herokuapp.com/'
+    apiBaseUrl: 'https://horror-catalogue-9e055c6b0745.herokuapp.com'
   };
